791dacc6bf3ec8e8445bd8f123fa63267a0e7819
Francielli Rodrigues <contato.franciellirodrigues@outlook.com>
2022-08-20 15:17:08 -0300

    novos ajustes

--- wp-content/themes/primeiro-tema/index.php
+++ wp-content/themes/primeiro-tema/index.php
@@ -2,12 +2,9 @@
 
 <main>
   <h1>Testando essa bagaça!</h1>
-<<<<<<< HEAD
-=======
 
   <?php get_template_part("components/banner"); ?>
 
->>>>>>> 2f3dc3af87965c6e8478ecc2f18051192e31f691
 </main>
 
 <?php get_footer(); ?>
