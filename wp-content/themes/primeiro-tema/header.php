73ef598e046c789b7055ea00aa28b2a3b6b733df
Francielli Rodrigues <contato.franciellirodrigues@outlook.com>
2022-08-20 18:41:51 -0300

    header finalizado

--- wp-content/themes/primeiro-tema/header.php
+++ wp-content/themes/primeiro-tema/header.php
@@ -5,11 +5,22 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="<?= get_template_directory_uri() ?>./assets/style.css">
+  <link rel="stylesheet" href="<?= get_template_directory_uri() ?>./assets/css/style.css">
   <?php wp_head() ?>
+  <title>Papelaria Cruz</title>
 </head>
 
 <body>
   <header>
-    <h1>Aqui é o header!</h1>
+    <nav class="container">
+      <div class="menu">
+       <img src="<?php echo get_theme_file_uri('assets/img/logo2.png')?>" alt="logo papelaria Cruz">
+        <ul>
+          <li><a class="title" href="#home">Inicio</a></li>
+          <li><a class="title" href="#about">Sobre</a></li>
+          <li><a class="title" href="#products">Produtos</a></li>
+          <li><a class="title" href="#contact">Contato</a></li>
+        </ul>
+      </div>
+    </nav>
   </header>
