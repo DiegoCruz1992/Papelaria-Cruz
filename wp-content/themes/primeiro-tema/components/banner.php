1e67169a6833ecb16cad50d95d74780331a9f2a2
Francielli Rodrigues <contato.franciellirodrigues@outlook.com>
2022-08-27 15:09:23 -0300

    adicionado footer e swiper

--- wp-content/themes/primeiro-tema/components/banner.php
+++ wp-content/themes/primeiro-tema/components/banner.php
@@ -1,5 +1,29 @@
 <section id="banner">
   <div class="banners">
-  <img class="banner" src="<?php echo get_theme_file_uri('assets/img/banner-01.webp')?>" alt="logo papelaria Cruz">
+  <!-- <img class="banner" src="<?php// echo get_theme_file_uri('assets/img/banner-01.webp')?>" alt="logo papelaria Cruz"> -->
+  <!-- Slider main container -->
+
+    <div class="swiper mySwiper">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+        <img src="<?php echo get_theme_file_uri('assets/img/banner-01.webp'); ?>" alt="">
+        </div>
+        <div class="swiper-slide">
+        <img src="<?php echo get_theme_file_uri('assets/img/banner-02.webp'); ?>" alt="">
+        </div>
+        <div class="swiper-slide">
+        <img src="<?php echo get_theme_file_uri('assets/img/banner-03.webp'); ?>" alt="">
+        </div>
+      </div>
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-pagination"></div>
+    </div>
+  </div>
+ 
+
+  <!-- If we need scrollbar -->
+  <div class="swiper-scrollbar"></div>
+</div>
   </div>
 </section>
--- wp-content/themes/primeiro-tema/header.php
+++ wp-content/themes/primeiro-tema/header.php
@@ -5,20 +5,33 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+      integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"
+/>
   <link rel="stylesheet" href="<?= get_template_directory_uri() ?>./assets/css/style.css">
   <?php wp_head() ?>
+
   <title>Papelaria Cruz</title>
 </head>
 
 <body>
-  <header>
+  <header id="home">
     <nav>
       <div class="menu">
         <!-- <img src="<?php //echo get_theme_file_uri('assets/img/logo2.png')
                         ?>" alt="logo papelaria Cruz"> -->
         <img class="img-logo" src="<?= get_field('logo_header'); ?>" alt="logo papelaria Cruz">
         <ul>
-          <li><a class="title" href="#home">Inicio</a></li>
+          
           <li><a class="title" href="#about">Sobre</a></li>
           <li><a class="title" href="#products">Produtos</a></li>
           <li><a class="title" href="#footer">Contato</a></li>
